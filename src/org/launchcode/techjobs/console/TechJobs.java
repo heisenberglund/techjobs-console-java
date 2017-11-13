ab24c7fb80035e5495ee4c2c369f46b696431382
Bill Volz <william.volz13@gmail.com>
2017-11-13 14:17:54 -0600

    working on printJobs

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -11,7 +11,7 @@ public class TechJobs {
 
     private static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -103,7 +103,7 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
@@ -111,6 +111,18 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs coming soon");
+
+
+        for (HashMap<String, String> job : someJobs) {
+            String jobInfo = "\n*****\n";
+            for (HashMap.Entry<String, String> jobColumn : job.entrySet()) {
+                jobInfo += jobColumn.getKey() + ": " + jobColumn.getValue() + ", " ;
+
+            }
+
+                System.out.println(jobInfo);
+
+
+        }
     }
 }
